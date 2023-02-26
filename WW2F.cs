016e542d5c83e29ae415873a919474d8aa10f904
SCARFIEEEE <125533603+SCARFIEEEE@users.noreply.github.com>
2023-02-26 07:00:14 -0800

    Add files via upload

---
+++ WW2F.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+    namespace weather_identifier
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            //loop
+            string type = "";
+            while (type != "Celcius" && type != "celcius" && type != "Farenheit" && type != "farenheit" && type != "Kelvin" && type != "kelvin")
+            {
+                Console.WriteLine("What type of temperature do you want? (Celcius, Farenheit, Kelvin)");
+                type = Console.ReadLine();
+
+                //celcius
+                if (type == "Celcius" || type == "celcius")
+                {
+                    Console.WriteLine("What is the temperature right now? (Celcius)");
+                    double temperaturec = Convert.ToDouble(Console.ReadLine());
+
+                    if (temperaturec <= 10)
+                    {
+                        Console.WriteLine("it is not advisable to go out, it's freezing cold outsude");
+                    }
+                    else if (temperaturec <= 20)
+                    {
+                        Console.WriteLine("You can go outside but wear thick clothing, it's chilly outside");
+                    }
+                    else if (temperaturec <= 30)
+                    {
+                        Console.WriteLine("Wear sunscreen, it's hot outside");
+                    }
+                    else
+                    {
+                        Console.WriteLine("It is also not advisable to go outside, it's burning outside");
+                    }
+                }
+
+                //farenheit
+                else if (type == "Farenheit" || type == "farenheit")
+                {
+                    Console.WriteLine("What is the temperature right now? (Farenheit)");
+                    double temperaturef = Convert.ToDouble(Console.ReadLine());
+
+                    if (temperaturef <= 50)
+                    {
+                        Console.WriteLine("it is not advisable to go out, it's freezing cold outsude");
+                    }
+                    else if (temperaturef <= 68)
+                    {
+                        Console.WriteLine("You can go outside but wear thick clothing, it's chilly outside");
+                    }
+                    else if (temperaturef <= 86)
+                    {
+                        Console.WriteLine("Wear sunscreen, it's hot outside");
+                    }
+                    else
+                    {
+                        Console.WriteLine("It is also not advisable to go outside, it's burning outside");
+                    }
+                }
+
+                //kelvin
+                else if (type == "Kelvin" || type == "kelvin")
+                {
+                    Console.WriteLine("What is the temperature right now? (Kelvin)");
+                    double temperaturek = Convert.ToDouble(Console.ReadLine());
+
+                    if (temperaturek <= 283.15)
+                    {
+                        Console.WriteLine("it is not advisable to go out, it's freezing cold outsude");
+                    }
+                    else if (temperaturek <= 293.15)
+                    {
+                        Console.WriteLine("You can go outside but wear thick clothing, it's chilly outside");
+                    }
+                    else if (temperaturek <= 303.15)
+                    {
+                        Console.WriteLine("Wear sunscreen, it's hot outside");
+                    }
+                    else
+                    {
+                        Console.WriteLine("It is also not advisable to go outside, it's burning outside");
+                    }
+                }
+
+                else
+                {
+                    Console.WriteLine("Please input a valid type of temperature.");
+                }
+            }
+        }
+    }
+}
---
+++ WW3.cs
@@ -0,0 +1,44 @@
+﻿//Situation 1
+Console.WriteLine("Enter your name: ");
+string name = Console.ReadLine();
+Console.WriteLine("Your name is: " + name);
+
+//Situation 2
+Console.WriteLine("Think of a number: ");
+int myNum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Your numbers is: " + myNum);
+
+//Situation 3
+Console.WriteLine("What is your name?");
+string username = Console.ReadLine();
+
+Console.WriteLine("What grade are you on?");
+int usergrade = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("And what strand are you currently studying?");
+string userstrand = Console.ReadLine();
+
+Console.WriteLine("From what section?");
+string usersection = Console.ReadLine();
+
+Console.WriteLine("Hello, " + username + " from " + userstrand + "-" + usergrade + usersection);
+
+//SItuation 4
+
+// Incorrect variables
+double number = 333.333;
+bool mybool = false;
+char name1 = 'A', name2 = 'B', name3 = 'C';
+string dinosaur = "T - rex";
+
+
+Console.WriteLine("Enter your name: ");
+string myname = Console.ReadLine();
+
+Console.WriteLine("What is your age: ");
+int age = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("How much do you weigh");
+double weight = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Hi, I'am " + myname + " and i'm " + age + " and I weigh " + weight);
